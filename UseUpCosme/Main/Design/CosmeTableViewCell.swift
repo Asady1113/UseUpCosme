041cbed014c72cd86b190bbb7571825f4df7b0e2
Asady1113 <asadyswift@gmail.com>
2022-12-20 15:45:47 +0900

    デザイン作成

---
+++ UseUpCosme/Main/Design/CosmeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CosmeTableViewCell.swift
+//  UseUpCosme
+//
+//  Created by 浅田智哉 on 2022/12/20.
+//
+
+import UIKit
+
+class CosmeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
