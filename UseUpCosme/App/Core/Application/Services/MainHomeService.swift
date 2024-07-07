f172310d2e7acd550912dd8b87ae0236fd096471
Asady1113 <asadyswift@gmail.com>
2024-07-07 23:59:36 +0900

    refactor: フォルダの作成

---
+++ UseUpCosme/App/Core/Application/Services/MainHomeService.swift
@@ -0,0 +1,12 @@
+//
+//  MainHomeService.swift
+//  UseUpCosme
+//
+//  Created by 浅田智哉 on 2024/07/07.
+//
+
+import Foundation
+
+class MainHomeService {
+    
+}
---
+++ UseUpCosme/App/Core/Application/protocols/MainHomeServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  IMainHomeService.swift
+//  UseUpCosme
+//
+//  Created by 浅田智哉 on 2024/07/07.
+//
+
+import Foundation
+
+protocol MainHomeServiceProtocol {
+    func selectCatgegory(_ tag: Int)
+}
